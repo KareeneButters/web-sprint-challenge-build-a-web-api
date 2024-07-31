366bd18e32d1e8c1643f472d5b4b4fbe6cffdb05
Kareene Butler <Kareenebutler5@gmail.com>
2024-07-31 19:19:29 -0400

    half way there

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,74 @@
-// Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Projects.get()
+        res.status(200).json(projects)
+    } catch (error) {
+        res.status(500).json({ message: "Error getting projects", error: error.message })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try{
+     const projectId = req.params.id
+     const projects = await Projects.get(projectId)
+     if(!projects) {
+        return res.status(404).json({ message: "no project with given id" })
+     }
+     res.status(200).json(projects)
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
+
+router.post('/', async (req, res) => {
+    try{
+        const newProject = req.body
+
+        if(!newProject || !newProject.name || !newProject.description) {
+            return res.status(400).json({ message: "Missing required fields" })
+        }
+        const project = await Projects.insert(newProject)
+        res.status(201).json(project)
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
+
+router.put('/:id', async(req, res) => {
+    try {
+        const projectId = req.params.id
+        const updatedProject = req.body
+
+        if (!updatedProject || !updatedProject.name || !updatedProject.description || typeof updatedProject.completed !== 'boolean') {
+            return res.status(400).json({ message: "Missing required fields or invalid data" })
+        }
+        const project = await Projects.update(projectId, updatedProject)
+        if (!project) {
+            return res.status(404).json({ message: "Project not found" })
+        }
+        res.status(200).json(project)
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const projectId = req.params.id
+        const count = await Projects.remove(projectId)
+        if (count > 0) {
+            res.status(204).end() 
+        } else {
+            res.status(404).json({ message: "Project not found" })
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const projectRouter = require('./projects/projects-router')
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(express.json())
+server.use('/api/projects', projectRouter)
 
 module.exports = server;
